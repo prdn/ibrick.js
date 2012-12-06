097edc9b18159ff9b7d498b06d1af17e3355f86b
Paolo <paolo.ardoino@gmail.com>
2012-12-06 21:34:32 +0100

    Initial import

---
+++ ibrick.js
@@ -0,0 +1,57 @@
+function ibrick() {};
+
+ibrick.prototype.init = function(conf) {
+	this._hooks = {};
+};
+
+ibrick.prototype.addHook = function(key, tag, deps, cb) {
+	if (this._hooks[key] === undefined) {
+		this._hooks[key] = [];
+	}
+	var hooks = this._hooks[key];
+	if (hooks.every(function(hook) {
+		if (hook.tag === tag) {
+			return false;
+		}
+		return true;
+	})) {
+		hooks.push({tag: tag, deps: deps, cb: cb});
+		return true;
+	}
+	return false;
+};
+
+ibrick.prototype.removeHook = function(key, tag) {
+	// NOT IMPLEMENTED
+};
+
+ibrick.prototype.runHook = function(key, input, output, complete) {
+	var self = this;
+
+	if (input === undefined) {
+		input = {};
+	}
+	if (output === undefined) {
+		output = {};
+	}
+	
+ 	var _hooks = this._hooks[key] || [];
+
+	var hooks = {
+		'@data': function(callback) {
+			callback(null, input);
+		},
+		'@out': function(callback) {
+			callback(null, output);
+		}
+	};
+
+	_hooks.every(function(hook) {
+		hooks[hook.tag] = ['@data', '@out'].concat(hook.deps || []).concat([hook.cb]);
+		return true;
+	});
+	
+	async.auto(hooks, function(err) {
+		complete(err, err ? undefined : output);
+	});
+};
