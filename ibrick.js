e1efb7a477070fd87958627755932f65b04986c5
Paolo Ardoino <paolo@Paolos-MacBook-Air.local>
2013-09-02 12:00:50 +0200

    Deps runtime check

--- ibrick.js
+++ ibrick.js
@@ -1,79 +1,97 @@
 var isClient = true;
 if (typeof exports !== 'undefined') {
-	if (typeof module !== 'undefined' && module.exports) {
-		isClient = false;
-	}
+    if (typeof module !== 'undefined' && module.exports) {
+	isClient = false;
+    }
 }
 
 if (!isClient) {
-	async = require("async");
+    async = require("async");
 }
 
 function ibrick() {};
 
 ibrick.prototype.init = function(conf) {
-	this._hooks = {};
+    this._hooks = {};
 };
 
 ibrick.prototype.addHook = function(key, tag, deps, cb) {
-	if (this._hooks[key] === undefined) {
-		this._hooks[key] = [];
+    if (this._hooks[key] === undefined) {
+	this._hooks[key] = [];
+    }
+    
+    if (tag === undefined) {
+	tag = ('id-' + Math.random()).replace('.', '');
+    }
+    
+    deps = deps || [];
+    
+    var hooks = this._hooks[key];
+    if (hooks.every(function(hook) {
+	if (hook.tag === tag) {
+	    return false;
 	}
-
-	if (tag === undefined) {
-		tag = ('id-' + Math.random()).replace('.', '')
-	}	
-
-	var hooks = this._hooks[key];
-	if (hooks.every(function(hook) {
-		if (hook.tag === tag) {
-			return false;
-		}
-		return true;
-	})) {
-		hooks.push({tag: tag, deps: deps, cb: cb});
-		return true;
-	}
-	return false;
+	return true;
+    })) {
+	hooks.push({ tag: tag, deps: deps, cb: cb });
+	return true;
+    }
+    throw new Error("IBRICK: hook tag already existing " + key + ' ' + tag);
 };
 
 ibrick.prototype.removeHook = function(key, tag) {
-	// NOT IMPLEMENTED
+    // NOT IMPLEMENTED
 };
 
 ibrick.prototype.runHook = function(key, input, output, complete) {
-	var self = this;
-
-	if (input === undefined) {
-		input = {};
+    var self = this;
+    
+    if (input === undefined) {
+	input = {};
+    }
+    if (output === undefined) {
+	output = {};
+    }
+    
+    var _hooks = this._hooks[key] || [];
+    
+    var hooks = {
+	'@data': function(callback) {
+	    callback(null, input);
+	},
+	'@out': function(callback) {
+	    callback(null, output);
 	}
-	if (output === undefined) {
-		output = {};
-	}
-	
- 	var _hooks = this._hooks[key] || [];
-
-	var hooks = {
-		'@data': function(callback) {
-			callback(null, input);
-		},
-		'@out': function(callback) {
-			callback(null, output);
+    };
+    
+    _hooks.every(function(hook) {
+	var deps = [],
+	    _deps = hook.deps;
+	_deps.every(function(dep) {
+	    var found = false;
+	    _hooks.every(function(_hook) {
+		if (hook.tag === dep) {
+		    found = true;
+		    return false;
 		}
-	};
-
-	_hooks.every(function(hook) {
-		hooks[hook.tag] = ['@data', '@out'].concat(hook.deps || []).concat([hook.cb]);
 		return true;
+	    });
+	    if (found) {
+		deps.push(dep);
+	    }
+	    return true;
 	});
-	
-	async.auto(hooks, function(err) {
-		if (typeof complete === 'function') {
-			complete(err, err ? undefined : output);
-		}
-	});
+	hooks[hook.tag] = ['@data', '@out'].concat(deps).concat([hook.cb]);
+	return true;
+    });
+    
+    async.auto(hooks, function(err) {
+	if (typeof complete === 'function') {
+	    complete(err, err ? undefined : output);
+	}
+    });
 };
 
 if (!isClient) {
-	module.exports = ibrick;
+    module.exports = ibrick;
 }
